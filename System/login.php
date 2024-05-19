f550e3b51e5df100172d519f8ff89f3c7035eae9
Lian-Cunanan <160018244+Lian-Cunanan@users.noreply.github.com>
2024-05-19 21:56:45 +0800

    fix the log in bugs

--- System/login.php
+++ System/login.php
@@ -261,7 +261,9 @@ include 'constants/check-login.php';
 
 <div class="modal-footer text-center">
 <button type="submit" class="btn btn-primary">Login</button>
-
+</div>
+										
+</div>
 </form>
 
 							  			<div id="forgotPasswordModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">
@@ -319,7 +321,105 @@ include 'constants/check-login.php';
 				</div>
 			
 			</div>
+			<footer class="footer-wrapper">
 			
+				<div class="main-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-12 col-md-9">
+							
+								<div class="row">
+								
+									<div class="col-sm-6 col-md-4">
+									
+										<div class="footer-about-us">
+											<h5 class="footer-title">About Hire Me Jobs</h5>
+											<p>Hire Me Jobs is a job portal, online job management system developed by BSCPE 2-A for his project in february 2018.</p>
+										
+										</div>
+
+									</div>
+									
+									<div class="col-sm-6 col-md-5 mt-30-xs">
+										<h5 class="footer-title">Quick Links</h5>
+										<ul class="footer-menu clearfix">
+											<li><a href="./">Home</a></li>
+											<li><a href="job-list.php">Job List</a></li>
+											<li><a href="employers.php">Employers</a></li>
+											<li><a href="employees.php">Employees</a></li>
+											<li><a href="contact.php">Contact Us</a></li>
+											<li><a href="#">Go to top</a></li>
+
+										</ul>
+									
+									</div>
+
+								</div>
+
+							</div>
+							
+							<div class="col-sm-12 col-md-3 mt-30-sm">
+							
+								<h5 class="footer-title">Hire Me Jobs Contact</h5>
+								
+								<p>Address : Takoradi, School Junction PO.BOX AX40</p>
+								<p>Email : <a href=":Hire Me.@gmail.com">Hire Me.nath2@gmail.com</a></p>
+								<p>Phone : <a href="tel:+233546607474">+233 546 607 474</a></p>
+								
+
+							</div>
+
+							
+						</div>
+						
+					</div>
+					
+				</div>
+				
+				<div class="bottom-footer">
+				
+					<div class="container">
+					
+						<div class="row">
+						
+							<div class="col-sm-4 col-md-4">
+					
+								<p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Nightingale Vision Software</p>
+								
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+							
+								<ul class="bottom-footer-menu">
+									<li><a >Developed by BSCPE 2-A</a></li>
+								</ul>
+							
+							</div>
+							
+							<div class="col-sm-4 col-md-4">
+								<ul class="bottom-footer-menu for-social">
+									<li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+									<li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+									<li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+								</ul>
+							</div>
+						
+						</div>
+
+					</div>
+					
+				</div>
+			
+			</footer>
+			
+		</div>
+
+
+	</div> 
+
 
 <div id="back-to-top">
    <a href="#"><i class="ion-ios-arrow-up"></i></a>
